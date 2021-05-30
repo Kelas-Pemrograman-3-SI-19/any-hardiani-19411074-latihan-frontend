7f2cefc3ab7e70f96e78fd69faa99a2023112cc1
anyanyhar24 <any.19411074@student.ubl.ac.id>
2021-05-30 03:01:15 -0700

    latihan 25 mei 2021

---
+++ src/pages/Index.vue
@@ -0,0 +1,55 @@
+<template>
+<div class="q-pa-md" style="max-width: 400px">
+
+    <q-form
+      @submit="onSubmit"
+      class="q-gutter-md"
+    >
+      <q-input
+        filled
+        v-model="form.namakegiatan"
+        label="Nama kegiatan"
+        :rules="[ val => val && val.length > 0 || 'Mohon isi nama kegiatan']"
+      />
+      <q-input
+        filled
+        v-model="form.deskripsi"
+        label="Deskripsi Kegiatan"
+        :rules="[ val => val && val.length > 0 || 'Mohon isi deskripsi kegiatan']"
+      />
+      <q-input
+        filled
+        v-model="form.waktu"
+        label="waktu Kegiatan"
+        :rules="[ val => val && val.length > 0 || 'Mohon isi waktu kegiatan']"
+      />
+
+      <div>
+        <q-btn label="Submit" type="submit" color="primary"/>
+      </div>
+    </q-form>
+
+  </div>
+</template>
+<script>
+
+export default {
+  data () {
+    return {
+      form: {
+        namakegiatan: null,
+        deskripsi: null,
+        waktu: null
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      this.$axios.post('kegiatan/insert', this.form)
+        .then(res => {
+          console.log(res)
+        })
+    }
+  }
+}
+</script>
